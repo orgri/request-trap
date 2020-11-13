30df9b05f58cdabcd60656612bbf33024cdb3bc0
Vladyslav Reva <juve1991@gmail.com>
2020-11-13 17:30:33 +0200

    feat: make frontend part pretty

--- public/js/socket-client.js
+++
@@ -1,9 +0,0 @@
-const socket = io();
-const room = window.location.pathname.split('/')[1];
-
-console.log('room name:', room);
-
-socket.emit('join', room);
-socket.on('messages', (data) => {
-  console.log(data);
-});
